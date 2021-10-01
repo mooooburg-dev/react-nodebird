0db2a7be307e6676bf16c9d332bc5887e757534b
mooooburg-dev <jeongmupark@gmail.com>
2021-10-01 10:27:37 +0900

    Redux 설치

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import PropTypes from "prop-types";
 import "antd/dist/antd.css";
+import wrapper from "../store/configureStore";
 
 const NodeBird = ({ Component }) => {
   return (
@@ -20,4 +21,4 @@ NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
