38aab0442c532a945efb5c86dc6c8280f2aa2c02
mooooburg-dev <jeongmupark@gmail.com>
2022-07-13 00:54:23 +0900

    팔로우, 언팔로우 구현

--- prepare/front/components/FollowButton.js
+++ prepare/front/components/FollowButton.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from 'antd';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { FOLLOW_REQUEST, UNFOLLOW_REQUEST } from '../reducers/user';
 
 const FollowButton = ({ post }) => {
-  return <Button>팔로우</Button>;
+  const dispatch = useDispatch();
+  const { me, followLoading, unfollowLoading } = useSelector(
+    (state) => state.user
+  );
+  const isFollowing = me && me?.Followings.find((v) => v.id === post.User.id);
+  const onClickButton = useCallback(() => {
+    if (isFollowing) {
+      dispatch({
+        type: UNFOLLOW_REQUEST,
+        data: post.User.id,
+      });
+    } else {
+      dispatch({
+        type: FOLLOW_REQUEST,
+        data: post.User.id,
+      });
+    }
+  }, [isFollowing]);
+  return (
+    <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
+      {isFollowing ? '언팔로우' : '팔로우'}
+    </Button>
+  );
 };
 
 FollowButton.propTypes = {
